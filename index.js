ca976956398eaeaabce2223b7ef4977b870cac64
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 13:48:31 -0400

    configured PG and env variables

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config(); // loads .env configuration
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 9000;
+const port = process.env.DEV_PORT;
 
-server.listen(port, () => console.log(`\n=== Running on port ${port} ===\n`));
+server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.DEV_PORT}, ${process.env.DB_USER}, ${process.env.DB_NAME}, ${process.env.DB_PASS} \n====================================================================\n`));
--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,8 @@ const localPg = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
 };
+
+// set dbConnection url or use localPg object is no DBURL found...
 const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
@@ -24,18 +26,18 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: dbConnection,
+    client: 'pg', // yarn add pg for this to work...
+    connection: dbConnection, // could be an obj or a string...
     pool: {
-      min: 2,
-      max: 10,
+      min: 2, // don't touch (DT)
+      max: 10,  // DT
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './db/migrations',
+      tableName: 'knex_migrations', // DT
+      directory: './db/migrations', // DT
     },
     seeds: {
-      directory: './db/seeds',
+      directory: './db/seeds', // DT
     },
   },
 };
