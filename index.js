767b5b9321d699b42e5b0c9b4774ede1558fb712
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 14:15:24 -0400

    changed PORT

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config(); // loads .env configuration
 
 const server = require('./api/server.js');
 
-const port = process.env.DEV_PORT || 9000;
+const port = process.env.PORT || 9000;
 
-server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.DEV_PORT}, DB: ${process.env.DB} \n====================================================================\n`));
+server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.PORT}, DB: ${process.env.DB} \n====================================================================\n`));
