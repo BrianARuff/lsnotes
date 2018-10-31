3d8aee3922008b14a6d58e700b661074ac70fd89
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 14:09:24 -0400

    updated .env

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config(); // loads .env configuration
 
 const server = require('./api/server.js');
 
-const port = process.env.DEV_PORT;
+const port = process.env.DEV_PORT || 9000;
 
 server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.DEV_PORT}, DB: ${process.env.DB} \n====================================================================\n`));
