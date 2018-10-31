9dbaa41167ecbbb907b256af0fc2baaedbf6bb0e
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 13:55:42 -0400

    changed index name

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const server = require('./api/server.js');
 
 const port = process.env.DEV_PORT;
 
-server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.DEV_PORT}, ${process.env.DB_USER}, ${process.env.DB_NAME}, ${process.env.DB_PASS} \n====================================================================\n`));
+server.listen(port, () => console.log(`\n====================================================================\n       Running on port ${process.env.DEV_PORT}, DB: ${process.env.DB} \n====================================================================\n`));
